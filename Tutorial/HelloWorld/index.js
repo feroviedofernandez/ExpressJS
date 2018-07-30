fb1b54a39cdac77371daacb65496684b02894065
Fernando Oviedo Fernández <feroviedofernandez@gmail.com>
2018-07-30 21:40:07 +0200

    Explicación callback de app.listen()

--- Tutorial/HelloWorld/index.js
+++ Tutorial/HelloWorld/index.js
@@ -5,4 +5,12 @@ app.get('/', function (req, res) {
    res.send("Hello World!");
 });
 
-app.listen(3000);
+console.log("Llamando a app.listen().");
+
+var server = app.listen(3000, function (){
+    console.log("Llamada a la función callback.");
+    var port = server.address().port;
+    console.log('Aplicación escuchando en el puerto %s', port);
+  });
+
+console.log("app.listen() ejecutado.");
